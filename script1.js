23f5fe19f7859df6989bfa384aff3b1bad1a581a
AyrenLKillman <ayrenkillman@gmail.com>
2024-08-21 14:23:39 -0500

    009 basic operatorz

---
+++ script1.js
@@ -0,0 +1,44 @@
+/*let boo = "cheese";
+console.log(6 + 3);
+multii line
+comment 
+let firstName = "bob";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Dishwasher";
+let myFavorieClass = "Coding";
+
+let favNumber = 12; // Number
+let lastName = "Joe"; //String
+let cannedBeans = true; //boolean
+let cheese //undefined / null
+
+*/
+
+// let isCheeseGood = true;
+// console.log(typeof (true));
+// console.log(typeof (isCheeseGood));
+// // console.log(typeof (21));
+// // console.log(typeof ("aaaa"));
+// isCheeseGood = "aaaa"
+// console.log(typeof (isCheeseGood));
+// let bruhMans;
+// console.log(bruhMans)
+// console.log(typeof (bruhMans));
+
+// bruhMans = "asdf"
+// console.log(typeof (bruhMans));
+// console.log(typeof (null));
+
+let pizza = "sausagerz" //let can be changed
+console.log(pizza)
+pizza = "peperz"
+console.log(pizza)
+const burger = 3 //const Cannot be changed
+var dude = 8 //dont use var, same as let but more buggy
+
+
+// on 009 Basic Operators
