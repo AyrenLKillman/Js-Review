2e63c239107af86f9fdb1bdf6394737841ceeaa6
AyrenLKillman <ayrenkillman@gmail.com>
2024-08-22 13:04:22 -0500

    random commit

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 /*let boo = "cheese";
 console.log(6 + 3);
 multii line
-comment 
+comment
 let firstName = "bob";
 
 console.log(firstName);
@@ -33,12 +33,25 @@ let cheese //undefined / null
 // console.log(typeof (bruhMans));
 // console.log(typeof (null));
 
-let pizza = "sausagerz" //let can be changed
-console.log(pizza)
-pizza = "peperz"
-console.log(pizza)
-const burger = 3 //const Cannot be changed
-var dude = 8 //dont use var, same as let but more buggy
+// let pizza = "sausagerz" //let can be changed
+// console.log(pizza)
+// pizza = "peperz"
+// console.log(pizza)
+// const burger = 3 //const Cannot be changed
+// var dude = 8 //dont use var, same as let but more buggy
+
+let currentYear = 2024
+const ageBob = currentYear - 1950;
+const ageJoe = currentYear - 1998;
+console.log(ageBob, ageJoe);
+
+console.log(ageBob * 2, ageJoe / 2, 2 ** 3)   // ** is used to make something to the power to something, so 2^3
+
+const nameOne = "bob";
+const nameTwo = "joe";
+console.log(nameOne + " " + nameTwo);
 
 
-// on 009 Basic Operators
+
+
+
